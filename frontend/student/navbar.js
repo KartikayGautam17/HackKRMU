02ca577ace93d968732e0a68f471c962faaad53f
kartikay <kartikaygautam24617@gmail.com>
2024-03-05 23:18:38 +0530

    abcd

--- frontend/student/navbar.js
+++ frontend/student/navbar.js
@@ -1,16 +1,20 @@
 let target = document.getElementById("verticalnavbar");
 let bars = document.querySelector(".fa.fa-bars")
 let times = document.querySelector(".fa.fa-times")
+const original_color = document.body.style.backgroundColor;
 function showMenu(){
     target.style.left = "0%";
     times.style.visibility = "visible";
     bars.style.visibility = "hidden";
+    let temp = target.style['backgroundColor'];
+    document.body.style['backgroundColor'] = "grey";
+    target.style.opacity = temp;
 }
 function hideMenu(){
     target.style.left = "-50%";
     times.style.visibility = "hidden";
     bars.style.visibility = "visible";
+    document.body.style['backgroundColor'] = original_color;
 }
-console.log(120);
 bars.addEventListener("click",showMenu);
 times.addEventListener("click",hideMenu);
